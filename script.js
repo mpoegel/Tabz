2b0727e9f68ceca72a190c241d205c7553fc283d
mpoegel <mpoegel@comcast.net>
2014-04-06 00:30:40 -0400

    SECTION HEADERZ ON THE UP HIGH

--- script.js
+++ script.js
@@ -26,22 +26,76 @@ function deleteMulti() {
 	
 }
 
+// returns the div class that the url should be placed in
+function classify(tab) {
+
+	var value = tab.url;
+	
+	if (value == "") { return "New Tab"; }
+	
+	if(value.search("www.") >= 0) {
+		value = value.substring(value.search("www.") + 4);
+	}
+	else if (value.search("://") >= 0) {
+		value = value.substring(value.search("://") + 3 );
+	}
+	
+	for (var i = 0; i < value.length; i++) {
+		if(value[i] == ".") {
+			return value.substring(0, i);
+		}
+	}
+	
+	console.log(value);
+	return value;
+		
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
 	
-	// document.addEventListener('click', deleteMulti);
+	document.getElementById('deleteButton').addEventListener('click', deleteMulti);
 	
+	console.log('hello world');
 	
 	chrome.tabs.query({}, function (tabs) {
 		for (var i = 0; i < tabs.length; i++) {
 			if (tabs[i] != null) {
-				var this_div = document.createElement('div');
-				this_div.id = 'tab_' + i;
-				this_div.className = 'tab_class';
-				this_div.addEventListener('click', tabSelect);
+				var tab_div = document.createElement('div');
+				tab_div.id = 'tab_' + i;
+				tab_div.className = 'tab_class';
+				tab_div.addEventListener('click', tabSelect);
 				var to_add = document.createTextNode(tabs[i].title);
-				this_div.appendChild(to_add);
-				document.body.appendChild(this_div);
+				tab_div.appendChild(to_add);
+				
+				// classify the current tab by url
+				var section_name = classify(tabs[i]);
+				var section_div;
+				
+				// if that classifcation doesn't exist then create it
+				if (document.getElementById(section_name) == null) {
+					
+					// create a header node for the div
+					var section_header = document.createTextNode(section_name);
+					var header_div = document.createElement('div');
+					header_div.id = section_name + '_header';
+					document.body.appendChild(header_div);
+					
+					// create the section node under the header that will get toggled
+					section_div = document.createElement('div');
+					section_div.id = section_name;
+					section_div.className = 'tab_group';
+					
+					section_div.appendChild(section_header);
+					
+					document.body.appendChild(section_div);
+					
+				}
+				// if it does then retrieve it
+				else { section_div = document.getElementById(section_name);  }
+				
+				// add the tab to the appropriate section
+				section_div.appendChild(tab_div);
 			}
 		}
 	});
