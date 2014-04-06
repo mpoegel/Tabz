18fc89e967a320daae17cbb6f25c36048c983f68
mpoegel <mpoegel@comcast.net>
2014-04-05 20:04:54 -0400

    yellow lollipop

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function main() {
 	
 }
 
-
+function tabSelect() {
+	alert(this.id);
+}
 
 
 // if (document.title.indexOf("Google") != -1) {
@@ -23,8 +25,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	for (var i = 0; i < tabs.length; i++) {
 		if (tabs[i] != null) {
 			var this_div = document.createElement('div');
+			this_div.id = 'tab_' + i;
+			this_div.class = 'tab_class';
+			this_div.addEventListener('click', tabSelect);
 			var to_add = document.createTextNode(tabs[i].title);
-			this_div.appendChild(to_add);	
+			this_div.appendChild(to_add);
 			document.body.appendChild(this_div);
 		}
 	}
