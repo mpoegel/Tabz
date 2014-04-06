d40b72017ef1069933c7fba2a468eda7640ff1c5
mpoegel <mpoegel@comcast.net>
2014-04-05 23:13:59 -0400

    zebra stripes and more mangos

--- script.js
+++ script.js
@@ -9,25 +9,42 @@ function tabSelect() {
 }
 
 
+function deleteMulti() {
+	
+	chrome.tabs.query( {}, function(tabs) {
+	
+		for (var i = 0; i < tabs.length; i++) {
+			for (var j = 0; j < tabs.length; j++) {
+				if (i == j) { continue; }
+				if (tabs[i].url == tabs[j].url) {
+					chrome.tabs.remove(tabs[j].id);
+					j--;
+				}
+			}
+		}
+	});
+	
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {
 	
+	// document.addEventListener('click', deleteMulti);
+	
+	
 	chrome.tabs.query({}, function (tabs) {
-	for (var i = 0; i < tabs.length; i++) {
-		if (tabs[i] != null) {
-			var this_div = document.createElement('div');
-			this_div.id = 'tab_' + i;
-			this_div.className = 'tab_class';
-			this_div.addEventListener('click', tabSelect);
-			var to_add = document.createTextNode(tabs[i].title);
-			this_div.appendChild(to_add);
-			document.body.appendChild(this_div);
+		for (var i = 0; i < tabs.length; i++) {
+			if (tabs[i] != null) {
+				var this_div = document.createElement('div');
+				this_div.id = 'tab_' + i;
+				this_div.className = 'tab_class';
+				this_div.addEventListener('click', tabSelect);
+				var to_add = document.createTextNode(tabs[i].title);
+				this_div.appendChild(to_add);
+				document.body.appendChild(this_div);
+			}
 		}
-	}
 	});
-	
-	
 
 
 });
--- testing.js
+++ testing.js
@@ -8,39 +8,46 @@ function tabSelect() {
 	
 }
 
-function deleteMulti(list)
-{
-	var removing = []; 
-	for(var i = 0; i < list.length; i++){
+function deleteMulti() {
+	
+	chrome.tabs.query( {}, function(tabs) {
+	
 		var removing = []; 
-		for(var j = 0; j < list.length; j++){
-			if(i == j){
-				continue;
+		for(var i = 0; i < tabs.length; i++){
+			var removing = []; 
+			for(var j = 0; j < tabs.length; j++){
+				if(i == j){
+					continue;
+				}
+				if(tabs[i].url == tabs[j].url){
+					removing.push(j.id);
+				}
+				
 			}
-			if(list[i].url == list[j].url){
-				removing.push(j);
-			}
-			
-		}
-		chrome.tabs.remove(removing);
-	}	
+			chrome.tabs.remove(removing);
+		}	
+		
+	}
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
 	
+	
+	
+	
 	chrome.tabs.query({}, function (tabs) {
-	for (var i = 0; i < tabs.length; i++) {
-		if (tabs[i] != null) {
-			var this_div = document.createElement('div');
-			this_div.id = 'tab_' + i;
-			this_div.class = 'tab_class';
-			this_div.addEventListener('click', tabSelect);
-			var to_add = document.createTextNode(tabs[i].title);
-			this_div.appendChild(to_add);
-			document.body.appendChild(this_div);
+		for (var i = 0; i < tabs.length; i++) {
+			if (tabs[i] != null) {
+				var this_div = document.createElement('div');
+				this_div.id = 'tab_' + i;
+				this_div.class = 'tab_class';
+				this_div.addEventListener('click', tabSelect);
+				var to_add = document.createTextNode(tabs[i].title);
+				this_div.appendChild(to_add);
+				document.body.appendChild(this_div);
+			}
 		}
-	}
 	});
 
 
