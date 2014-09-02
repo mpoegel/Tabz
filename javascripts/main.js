55eb4955c93c6d4aa7f922e7ac5278daf05a927f
Matt Poegel <mpoegel@comcast.net>
2014-09-01 23:25:35 -0400

    added review section
    (populated manually)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+
+function add_stars(review_num, num_stars) {
+  for (i=0; i<num_stars; i++) {
+    $('#'+review_num).append('<img class="review-star" width="16px" src="images/star.png">');
+  }
+}
