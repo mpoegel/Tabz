09e9a0bd4d0f33e4a0b5dfc4d7c3c56f7dbd0773
Batboa <andrewbatbouta@gmail.com>
2015-03-14 15:38:36 -0400

    Options page Styled and Bug fixes

--- javascript/options_script.js
+++ javascript/options_script.js
@@ -1,21 +1,20 @@
-function StorageTest() {
-
-	var url = "key12313";
-		chrome.storage.local.get(url, function(result) {	
-			console.log(result);
-	});
-}
-
 function write_storage(removed){
 	var instance = $('#Storage_Removal');
 	var url = "key12313";
 		chrome.storage.local.get(url, function(result) {	
 			try{
 				for(var i = 0; i < result[url].length; i++){
-					if(($.inArray(result[url][i][result[url][i].length-1], removed))){
-						var button = '<li><button class="removal_buttons" id="' + result[url][i][result[url][i].length-1].replace(/ /g, "_") + "___TABZUSE" + '"  >' +  result[url][i][result[url][i].length-1] + ' </button></li>'
-						instance.append(button);
-					}
+						place = true;
+						for(var j = 0; j < removed.length; j++){
+							if(removed[j].trim() == result[url][i][result[url][i].length-1].trim()){
+								place = false;
+							}
+						}
+						if(place){
+							var button = '<li><button class="removal_buttons" id="' + result[url][i][result[url][i].length-1].replace(/ /g, "_") + "___TABZUSE" + '"  >' +  result[url][i][result[url][i].length-1] + ' </button></li>'
+							instance.append(button);
+						}
+					
 				}
 			}
 			catch(err){}
@@ -27,10 +26,8 @@ function remove_storage(name){
 	var new_name = String(name);
 	var index = -1;
 	new_name = name.substring(0, name.length - 1);
-	console.log(new_name);
 	chrome.storage.local.get(url, function(result) {	
 		for(var i = 0; i < result[url].length; i++){
-			console.log(result[url][i][result[url][i].length-1].trim());
 			if(result[url][i][result[url][i].length-1].trim() == new_name.trim()){
 				index = i;
 			}
@@ -51,12 +48,10 @@ function clear_stored(){
 
  $(document).ready(function() {
 	var removed = [];
-	$('#StorageButton').click(StorageTest);
 	write_storage(removed);
 	$(document).on('click', '.removal_buttons', function() {
 		var obj_id = this.id;
 		var to_close = $('#'+obj_id).html();
-		console.log('#'+obj_id)
 		remove_storage(to_close);
 		$( '#'+obj_id).remove();
 		removed.push(to_close.trim());
